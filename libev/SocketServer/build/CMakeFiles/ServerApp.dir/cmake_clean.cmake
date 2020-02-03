57641606bcdb1c08912c51093d799f52ac92c40b
Jeff Ma <maxiaolongjob@163.com>
2020-02-03 04:21:29 -0800

    ==cmake

---
+++ libev/SocketServer/build/CMakeFiles/ServerApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerApp.dir/src/LibevSockt_Server.c.o"
+  "ServerApp.pdb"
+  "ServerApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ServerApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ libev/SocketServer/build/CMakeFiles/TestClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestClient.dir/src/client.c.o"
+  "TestClient.pdb"
+  "TestClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
